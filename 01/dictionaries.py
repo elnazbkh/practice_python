55d4c3f84341c7bfa9d728f39564fc4ca41f2b2a
elnazbkh <elnaz.bakhtiari20@gmail.com>
2019-05-29 12:20:23 +0430

    tamrin_dictionary

---
+++ 01/dictionaries.py
@@ -0,0 +1,13 @@
+#movie = ["sunlight", "woody allen", 2009,"yes", "no"]
+movie ={"title":"sunlight", "director": "woody allen", "year": 2009, "answer": "yes"}
+print(movie["title"])
+
+#print(movie.clear())
+print(movie.values())
+print(movie.__len__())
+print(movie.get("name"))
+movie.update({"a": 1235, "b":585})
+print(movie)
+print(movie.get("writer", "unknown"))
+print(movie.keys())
+print(list(movie.keys()))
