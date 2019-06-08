0eed5812e9d082ffcd7eeb06c43f8bf3ab834b38
elnazbkh <elnaz.bakhtiari20@gmail.com>
2019-06-08 11:54:27 +0430

    tamrin_day_and_month

--- 01/day_and_month.py
+++ 01/day_and_month.py
@@ -1,15 +1,19 @@
 # date based on day number
+
 daynum = int(input("enter number of day:"))
+
 Day = 0
 Month = 0
 
 if 0 < daynum < 186:
     Month = (daynum // 31) + 1
     Day = daynum % 31
+
 elif 186 < daynum < 365:
     daynum -= 186
     Month = (daynum // 30) + 7
     Day = daynum % 30
+
 else:
     print("the number is out of range")
 
