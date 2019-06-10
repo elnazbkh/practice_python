1a1a7f78b6a7c392deec9f7baaf1cfbb1cd4d254
elnazbkh <elnaz.bakhtiari20@gmail.com>
2019-06-10 14:28:21 +0430

    tamrin_for_and_continue_odd

--- 02/for_and_countinue.py
+++ 02/for_and_countinue.py
@@ -1,8 +1,12 @@
 
-for i in ("a", "b", "c", "d", "e"):
-    if i == "c":
+# for i in ("a", "b", "c", "d", "e"):
+    #if i == "c":
+        #continue
+    #print("the letter is: ", i)
+
+#print("The end")
+
+for num in range (2,51):
+    if num % 2 == 0:
         continue
-    print("the letter is: ", i)
-
-print("The end")
-
+    print(num)
